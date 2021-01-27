fb0d5e42f5d18dd957ccfb58883cab603a9684bb
Logant08 <logantillman@ymail.com>
2021-01-27 10:15:12 -0500

    feat: Add Min.java

---
+++ hw1/Min.java
@@ -0,0 +1,19 @@
+public class Min {
+    public Min(String[] strings) {
+        printMinimum(strings);
+    }
+
+    private void printMinimum(String[] strings) {
+        String minString = strings[0];
+        for (String currString : strings) {
+            if (currString.compareTo(minString) < 0) {
+                minString = currString;
+            }
+        }
+        System.out.println(minString);
+    }
+
+    public static void main(String[] args) {
+        new Min(args);
+    }
+}
