6ab7a71959b058b8373fb21f41453006b9c9ad36
Logant08 <logantillman@ymail.com>
2021-01-31 18:40:18 -0500

    feat: Add ChineseZodiac.java

---
+++ hw1/ChineseZodiac.java
@@ -0,0 +1,23 @@
+public enum ChineseZodiac {
+    RAT (2020),
+    BULL (2009),
+    TIGER (2010),
+    RABBIT (2011),
+    DRAGON (2012),
+    SNAKE (2013),
+    HORSE (2014),
+    GOAT (2015),
+    MONKEY (2016),
+    COCK (2017),
+    DOG (2018),
+    PIG (2019);
+
+    private final int year;
+    private ChineseZodiac (int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+}
