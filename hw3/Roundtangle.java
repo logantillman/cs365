11a892018240da606c22cb53bd5a888846173485
logant08 <logantillman@ymail.com>
2021-02-15 18:18:22 -0500

    WIP: Define default fields

--- hw3/Roundtangle.java
+++ hw3/Roundtangle.java
@@ -1,3 +1,29 @@
 public class Roundtangle {
-    
+    public enum ArcSize {
+        SMALL (10),
+        MEDIUM (20),
+        LARGE (30);
+
+        private final int pixels;
+
+        ArcSize(int pixels) {
+            this.pixels = pixels;
+        }
+
+        public int getSize() {
+            return this.pixels;
+        }
+    }
+
+    int left = 0;
+    int top = 0;
+    int width = 20;
+    int height = 20;
+    int x1 = 0;
+    int y1 = 0;
+    int x2 = 20;
+    int y2 = 20;
+    // color = black;
+    String string = "";
+    ArcSize arcSize = ArcSize.MEDIUM;
 }
