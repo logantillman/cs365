94fc20c6bd5a6357810932c824b8004030b79500
logant08 <logantillman@ymail.com>
2021-02-15 17:14:20 -0500

    feat: Implement Queue interface

--- hw3/ArrayQueue.java
+++ hw3/ArrayQueue.java
@@ -1,5 +1,17 @@
 import java.util.ArrayList;
 
 public class ArrayQueue implements Queue {
+    ArrayList<Object> arrayList = new ArrayList<Object>();
 
+    void add(Object o) {
+        arrayList.add(o);
+    }
+
+    Object remove() {
+        return arrayList.remove(0);
+    }
+
+    boolean isEmpty() {
+        return arrayList.isEmpty();
+    }
 }
