1f9b6ab2f35d52f08ffce76daee7ada524a3f5fa
logantillman <logantillman@ymail.com>
2021-03-05 18:20:21 -0500

    WIP: Upload progress

--- hw5/queue.c
+++ hw5/queue.c
@@ -1,30 +1,49 @@
 #include "queue.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct Queue {
-		void *front;
-		void *back;
-		int size;
-		void **array;
+	void *front;
+	void *back;
+	int maxSize;
+	int size;
+	void **array;
 } myQueue;
 
-void* queue_new(int numItems) {
+void *queue_new(int numItems) {
+	printf("Created the queue\n");
 	myQueue *newQueue = (myQueue *) malloc(sizeof(myQueue));
-	newQueue->size = numItems;
+	newQueue->maxSize = numItems;
 	newQueue->array = (void *) malloc(sizeof(void *) * numItems);
+	newQueue->size = 0;
 	newQueue->front = newQueue->array[0];
-	newQueue->back = newQueue->array[numItems - 1];
 	return (void *) newQueue;
 }
 
 void queue_enqueue(void *q, void *item) {
-	
+	printf("Created the queue\n");
+	myQueue *queue = (myQueue *) q;
+	queue->array[queue->size] = item;
+	queue->back = queue->array[queue->size];
+	queue->size++;
 }
 
-void* queue_dequeue(void *q) {
-
+void *queue_dequeue(void *q) {
+	printf("Created the queue\n");
+	myQueue *queue = (myQueue *) q;
+	void *rv = queue->back;
+	queue->back = queue->array[queue->size - 2];
+	queue->size--;
+	return rv;
 }
 
 int queue_isEmpty(void *q) {
-
+	printf("Created the queue\n");
+	myQueue *queue = (myQueue *) q;
+	if (queue->size == 0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
