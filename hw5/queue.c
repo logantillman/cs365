8beb83bd278e2e6e145ffb898d4e90f7b89bbf47
logantillman <logantillman@ymail.com>
2021-03-06 00:38:24 -0500

    chore: Clean up commented code

--- hw5/queue.c
+++ hw5/queue.c
@@ -1,3 +1,5 @@
+// Author: Logan Tillman
+
 #include "queue.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,34 +13,48 @@ typedef struct Queue {
 } myQueue;
 
 void *queue_new(int numItems) {
-	printf("Created the queue\n");
 	myQueue *newQueue = (myQueue *) malloc(sizeof(myQueue));
 	newQueue->maxSize = numItems;
 	newQueue->array = (void *) malloc(sizeof(void *) * numItems);
 	newQueue->size = 0;
-	newQueue->front = newQueue->array[0];
 	return (void *) newQueue;
 }
 
 void queue_enqueue(void *q, void *item) {
-	printf("Created the queue\n");
 	myQueue *queue = (myQueue *) q;
+	
+	if (queue->size == queue->maxSize) {
+		return;
+	}
+
+	if (queue->size == 0) {
+		queue->front = item;
+	}
+	
 	queue->array[queue->size] = item;
 	queue->back = queue->array[queue->size];
 	queue->size++;
 }
 
 void *queue_dequeue(void *q) {
-	printf("Created the queue\n");
 	myQueue *queue = (myQueue *) q;
-	void *rv = queue->back;
-	queue->back = queue->array[queue->size - 2];
+	
+	void *rv = queue->front;
+
+	int i;
+	for (i = 1; i < queue->size; i++) {
+		queue->array[i - 1] = queue->array[i];
+	}
+
+	if (queue->size > 0) {
+		queue->front = queue->array[0];
+	}
+
 	queue->size--;
 	return rv;
 }
 
 int queue_isEmpty(void *q) {
-	printf("Created the queue\n");
 	myQueue *queue = (myQueue *) q;
 	if (queue->size == 0) {
 		return 1;
