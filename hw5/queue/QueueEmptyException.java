90644c4a7d05591578522631a59db81b72256377
Logant08 <logantillman@ymail.com>
2021-03-05 22:34:26 -0500

    feat: Add getMessage function

--- hw5/queue/QueueEmptyException.java
+++ hw5/queue/QueueEmptyException.java
@@ -6,4 +6,8 @@ public class QueueEmptyException extends Exception {
     public QueueEmptyException() {
         System.out.println("queue empty: cannot dequeue");
     }
+
+    public String getMessage() {
+        return new String("queue empty: cannot dequeue");
+    }
 }
