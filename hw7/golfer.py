127a56affd214bc9f5fc7118c8bbcfe7e8556510
logantillman <logantillman@ymail.com>
2021-04-01 13:32:50 -0400

    feat: Initial commit

---
+++ hw7/golfer.py
@@ -0,0 +1,5 @@
+class Golfer:
+    def __init__(lastName, score, handicap):
+        self.lastName = lastName
+        self.score = score
+        self.handicap = handicap
---
+++ hw7/handicap.py
@@ -0,0 +1,8 @@
+# Author: Logan Tillman
+
+import sys
+
+scoreFile = sys.argv[1]
+courseFile = sys.argv[2]
+
+print(scoreFile, courseFile)
