5c11eb9f6b48ed08989fe46a4c3a3995f511d521
logantillman <logantillman@ymail.com>
2021-04-01 23:18:54 -0400

    feat: Finish hw 7

--- hw7/golfer.py
+++ hw7/golfer.py
@@ -1,9 +1,54 @@
+# Author: Logan Tillman
+
 class Golfer:
     def __init__(self, lastName):
         self.lastName = lastName
         self.scores = []
         self.times = []
+        self.courses = []
 
-    def addScore(self, score, time):
+    # Adding information to the lists
+    def addScore(self, score, time, course):
         self.scores.append(score)
         self.times.append(time)
+        self.courses.append(course)
+
+    # Sorting the lists based on time
+    def sortLists(self):
+        self.sortedScores = [x for (y, x) in sorted(zip(self.times, self.scores), key=lambda pair: pair[0])]
+        self.sortedCourses = [x for (y,x) in sorted(zip(self.times, self.courses), key=lambda pair: pair[0])]
+
+    # Calculating the golfer's handicap
+    def calculateHandicap(self, courseDict):
+
+        # Grabbing the 20 most recent scores
+        scoresList = self.sortedScores[-20:]
+        coursesList = self.sortedCourses[-20:]
+
+        diffList = []
+
+        # Calculating the differential and adding it to the list
+        for score, course in zip(scoresList, coursesList):
+            rating = courseDict[course].rating
+            slope = courseDict[course].slope
+            differential = (score - rating) * 113 / slope
+            diffList.append(differential)
+
+        # Sorting the differentials and grabbing the 10 lowest
+        sortedDiffs = sorted(diffList)
+        sortedDiffs = sortedDiffs[:10]
+        
+        # Calculating the average of the differentials
+        sumDiffs = 0
+        for diff in sortedDiffs:
+            sumDiffs += diff
+        sumDiffs /= 10
+
+        # Returning 96% of the average 
+        return (sumDiffs * .96)
+        
+class Course:
+    def __init__(self, name, rating, slope):
+        self.name = name
+        self.rating = rating
+        self.slope = slope
--- hw7/handicap.py
+++ hw7/handicap.py
@@ -1,14 +1,17 @@
 # Author: Logan Tillman
 
-from golfer import Golfer
+from golfer import Golfer, Course
 import sys
 import datetime
 
+# Reading the command line arguments for the files
 scoreFile = sys.argv[1]
 courseFile = sys.argv[2]
 
+# Creating the empty golfer dictionary
 golferDict = {}
 
+# Reading the score file
 with open(scoreFile, 'r') as f:
     for line in f:
         splitLine = line.split()
@@ -17,13 +20,14 @@ with open(scoreFile, 'r') as f:
         year = splitLine[2]
         time = datetime.datetime(int(year), int(month), int(day))
         name = splitLine[3]
-        score = splitLine[4]
+        score = int(splitLine[4])
         courseArray = splitLine[5:]
         course = courseArray[0]
-        for word in courseArray:
-            if course != word:
-                course += " " + word
-        print(time, name, score, course)
+
+        # Building the course name
+        for i, word in enumerate(courseArray):
+            if i != 0:
+                course += ' ' + word
 
         # Check the dictionary to see if the golfer exists
         golfer = golferDict.get(name, 0)
@@ -33,8 +37,52 @@ with open(scoreFile, 'r') as f:
             golfer = Golfer(name)
             golferDict.update({name: golfer})
         
-        # If the golfer already exists, we add the score and time to their lists
-        else:
-            golfer.addScore(score, time)
-        print(golfer.lastName, golfer.scores, golfer.times)
+        # Adding the score, time, and course to the golfer's lists
+        golfer.addScore(score, time, course)
         
+# Creating the empty course dictionary
+courseDict = {}
+
+# Reading the course file
+with open(courseFile, 'r') as f: 
+    courseName = ''
+    rating = 0
+    slope = 0
+    for line in f:
+        splitLine = line.split()
+
+        # Checking to make sure the list isn't empty
+        if len(splitLine) > 0:
+
+            # If we're reading the 'Course' line, concatenate the course name together
+            if splitLine[0] == 'Course':
+                courseName = splitLine[1]
+                for i, word in enumerate(splitLine):
+                    if i != 0 and i != 1:
+                        courseName += ' ' + word
+            
+            # If we're reading the 'Rating' line, create an instance of the course class and store it in the dictionary
+            elif splitLine[0] == 'Rating':
+                rating = float(splitLine[1])
+                slope = float(splitLine[3])
+                course = Course(courseName, rating, slope)
+                courseDict.update({courseName: course})
+
+# Create our empty handicap and golfer lists
+handicapList = []
+golferList = []
+
+# Calculate the handicaps for each golfer
+for golfer in golferDict:
+    golferDict[golfer].sortLists()
+    handicapList.append(golferDict[golfer].calculateHandicap(courseDict))
+    golferList.append(golferDict[golfer].lastName)
+
+# Sort the handicap and golfer lists
+sortedGolfers = [x for (y, x) in sorted(zip(handicapList, golferList), key=lambda pair: pair[0])]
+sortedHandicaps = sorted(handicapList)
+
+# Print out the handicaps with their respective golfer in ascending order
+for handicap, golfer in zip(sortedHandicaps, sortedGolfers):
+    output = '{0:5.2f} {1:s}'.format(handicap, golfer)
+    print(output)
