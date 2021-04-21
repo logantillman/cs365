00953909040da22a619537e088d4e7b5a9b72efa
logantillman <logantillman@ymail.com>
2021-04-21 19:49:57 -0400

    feat: Finish avg

--- hw9/avg.scm
+++ hw9/avg.scm
@@ -1,4 +1,7 @@
-(define avg (lambda (L)
+(define (sum L)
     (cond
         ((null? L) 0)
-        (#t (+ (car L) (avg (cdr L)))))))
+        (#t (+ (car L) (sum (cdr L))))))
+
+(define (avg L)
+    (/ (* (sum L) 1.0) (length L)))
