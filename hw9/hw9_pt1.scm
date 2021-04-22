9eff3c0f3cd1632277162801609ea914d2c30602
logantillman <logantillman@ymail.com>
2021-04-21 21:31:07 -0400

    feat: Create submission file

---
+++ hw9/hw9_pt1.scm
@@ -0,0 +1,30 @@
+; swap.scm
+(define (swap x y)
+    (if (< x y)
+        (cons x y)
+        (cons y x)))
+
+; avg.scm
+(define (sum L)
+    (cond
+        ((null? L) 0)
+        (#t (+ (car L) (sum (cdr L))))))
+
+(define (avg L)
+    (cond 
+        ((null? L) 0)
+        (#t (/ (* (sum L) 1.0) (length L)))))
+
+; min.scm
+(define min
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      ((null? (cdr l)) (car l))
+      (#t (minHelper (cdr l) (car l))))))
+
+(define (minHelper l x)
+    (if (< (length l) 2) 
+        (if (< (car l) x) (car l) x)
+        (minHelper (cdr l) (let ((a (car l)))
+                            (if (< a x) a x)))))
